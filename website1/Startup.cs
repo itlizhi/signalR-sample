12e4cab4ac29103a9753c007e18f6edfc665bd2e
lizhi <itlizhi@163.com>
2017-02-21 18:43:42 +0800

    项目搭建

---
+++ website1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(website1.Startup))]
+namespace website1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
